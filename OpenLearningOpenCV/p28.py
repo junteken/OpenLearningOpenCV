6d78db00a81291703f3bdd1e699176253bba4088
admin <junteken@gmail.com>
2016-11-28 19:55:57 +0900

    first week code

---
+++ OpenLearningOpenCV/p28.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+# Draw a diagonal blue line with thickness of 5 px
+img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+img = cv2.polylines(img,[pts],True,(0,255,255))
+
+cv2.imshow('image', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
